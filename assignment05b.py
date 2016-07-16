4fea0dd34c9cd0630275ab286dc8b1bf389a2dda
Prabh Simran Singh Baweja <prabh@codenation.co.in>
2016-07-16 21:23:55 +0400

    assignment modified

--- assignment05b.py
+++ assignment05b.py
@@ -7,32 +7,40 @@ import os
 
 import pymysql
 import time
+import logging
 
-conn = boto.sqs.connect_to_region(
-    "us-east-1",
-     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-myqueue = conn.get_queue('cnu2016_prabh_simran_assignment05a')
-engine = create_engine("mysql+pymysql://root:prabh@localhost/cnu2016_prabh_simran")
-connection = engine.connect()
+def main():
+    logger = logging.getLogger()
 
-metadata = MetaData()
 
-AuditLog = Table('AuditLog', metadata, autoload=True, autoload_with=engine)
-log_id = 1
+    conn = boto.sqs.connect_to_region(
+        "us-east-1",
+         aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+         aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    myqueue = conn.get_queue(os.environ['my_queue'])
+    engine = create_engine(os.environ['my_db'])
+    connection = engine.connect()
 
-while 1:
-    print "... waiting messages ..."
-    messages = myqueue.get_messages(wait_time_seconds=20, num_messages=10)
-    for message in messages:
-        d = ast.literal_eval(message._body)
-        try:
-            ins = AuditLog.insert().values(id=log_id, start_time=d['date'], request_url=d['url'], response_code=d['responseCode'], ip_address=d['ipAddress'])
-            log_id = log_id + 1
-            connection.execute(ins)
-            myqueue.delete_message(message)
-        except:
-            print "Error"
-    time.sleep(1)
+    metadata = MetaData()
 
-connection.close()
+    AuditLog = Table('AuditLog', metadata, autoload=True, autoload_with=engine)
+    log_id = 1
+
+    while 1:
+        messages = myqueue.get_messages(wait_time_seconds=20, num_messages=10)
+        for message in messages:
+            d = ast.literal_eval(message._body)
+            try:
+                ins = AuditLog.insert().values(id=log_id, start_time=d['date'], request_url=d['url'], response_code=d['responseCode'], ip_address=d['ipAddress'])
+                log_id = log_id + 1
+                connection.execute(ins)
+                myqueue.delete_message(message)
+            except:
+                logger.error("error")
+
+        time.sleep(1)
+
+    connection.close()
+
+if __name__ == "__main__":
+    main()
