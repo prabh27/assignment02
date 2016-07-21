243f4e5d3b56b93d3186917cabd7bdff5330f84f
Prabh Simran Singh Baweja <prabh@codenation.co.in>
2016-07-22 01:45:14 +0400

    assignment06

--- MyProjects/ecommerce/serializers.py
+++ MyProjects/ecommerce/serializers.py
@@ -107,7 +107,8 @@ class OrdersSerializer(serializers.ModelSerializer):
                     print "aaya"
                     instance.customer = Customers.objects.get_or_create(customer_name=validated_data.get('customer')['customer_name'])[0]
                 else:
-                    instance.customer = Customers.objects.create(customer_name="null")
+                    if(instance.customer == None):
+                        instance.customer = Customers.objects.create(customer_name="null")
                 if validated_data.get('customer').get('address_line1'):
                     print validated_data.get('customer').get('address_line1')
                     instance.customer.address_line1 = validated_data.get('customer').get('address_line1')
